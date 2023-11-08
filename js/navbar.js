1eddb2bd44de7154b9b594cca5f8fc1334774210
CarolinaAyala <carolina.ayala1031@gmail.com>
2023-11-07 23:53:02 -0600

    se modifica background, se corrige tamaño de la letra en navbar y se modifica tamaño del logo en navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,9 @@
 function createNavbar (){
-    let navbar = document.getElementById("navbar");
+    let navbar = document.getElementById("navbar"); 
     let navigation = `
         <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #294BF3;" >
             <a class="navbar-brand" href="./index.html">
-                <img src="./src/logoAMR.png" alt="Logo AMR" width="30" height="24">
+                <img src="./src/logoAMR.png" alt="Logo AMR" width="70" height="70" border-radius= 40 px;>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
