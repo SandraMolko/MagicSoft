695a7fa3664796c4eb7c5f50a43a6d8d4da40531
angelavg <angelaverag@gmail.com>
2023-11-09 21:52:21 -0600

    se modifico cloudinary y el formulario

--- js/cloudinary.js
+++ js/cloudinary.js
@@ -1,14 +1,19 @@
-{<script type="text/javascript">  
-var myWidget = cloudinary.createUploadWidget({
+import {Cloudinary} from "@cloudinary/url-gen";
+
+const cld = new Cloudinary({cloud: {cloudName: 'dopadpbzu'}});
+
+const api_key = "495347973491413"
+
+// type="text/javascript">  
+ myWidget = Cloudinary.createUploadWidget({
   cloudName: 'dopadpbzu', 
   uploadPreset: 'my_preset'}, (error, result) => { 
     if (!error && result && result.event === "success") { 
       console.log('Done! Here is the image info: ', result.info); 
     }
-  }
-)
+}
+ )
 
 document.getElementById("upload_widget").addEventListener("click", function(){
     myWidget.open();
   }, false);
-</script> }
