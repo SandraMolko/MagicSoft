3e4ddb12080ab1ee021541ff78c5932461bc3147
Karen Valenzuela Landero <11952598+karenvalenzuelalandero@users.noreply.github.com>
2023-11-15 00:10:16 -0600

    Se corrigen las validaciones

--- js/registro.js
+++ js/registro.js
@@ -1,5 +1,4 @@
-const alert = document.getElementById("alert_container");
-
+const alert = document.getElementById("alert-container");
 
 function validarCampos() {
     const nombre = document.getElementById("nombre").value;
@@ -8,7 +7,6 @@ function validarCampos() {
     const contraseña = document.getElementById("contraseña").value;
     const contraseñaconf = document.getElementById("contraseñaconf").value;
     
-  
     let msj_error;
     if (nombre.length < 7) {
       msj_error = "Por favor escribe tu nombre completo";
@@ -16,64 +14,74 @@ function validarCampos() {
       return false;
     }
     
-    let telefonoRegEx = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
-    if(telefonoRegEx.test(telefono)|| isNaN(telefono) || telefono.length != 10){
-        msj_error="El formato del correo es incorrecto";
+    let telefonoRegEx = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4}$/
+    if(!telefonoRegEx.test(telefono) ){
+      console.log(telefonoRegEx.test(telefono));
+        msj_error="El formato del teléfono es incorrecto";
         showErrorMessage(msj_error);
         return false;
     }
 
     let emailRegEx = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/
-    if(emailRegEx.test(email)|| email.length < 6){
+    if(!emailRegEx.test(email)|| email.length < 6){
         msj_error="Por favor, verifica tu correo electrónico";
         showErrorMessage(msj_error);
         return false;
     }
 
     /*Especificaciones: mínimo 8 caracteres, una mayúscula, una minúscula, un número y un caracter especial*/
-  
     let contraseñaRegEx = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/
-    if(contraseñaRegEx.test(contraseña)|| contraseña.length < 8){
-        msj_error="La contraseña debe contener: mínimo 8 caracteres, una mayúscula, una minúscula, un número y un caracter especial.";
+    if(!contraseñaRegEx.test(contraseña)){
+        msj_error="La contraseña debe contener: mínimo 8 caracteres, una mayúscula, una minúscula, un número y un caracter especial (#?!@$ %^&*-).";
         showErrorMessage(msj_error);
         return false;
     }
-    /*Contraseña*/
-
-    if (contraseñaconf.value == contraseña.value) {
+    if (contraseñaconf.value !== contraseña.value) {
       msj_error = "Verifica tu contraseña";
       showErrorMessage(msj_error);
       return false;
     } return true
   }
+
   function showErrorMessage (msj_error){
     let mostrarAlerta = `<div class="alert alert-warning alert-dismissible show" role="alert" id="alert">
-    ${msj_error}
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" id="close-warning"></button>
-</div>`;
-  
-  alert.insertAdjacentHTML("beforeend",mostrarAlerta)
+      ${msj_error}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" id="close-warning"></button>
+      </div>`;
+    
+    alert.insertAdjacentHTML("beforeend",mostrarAlerta)
+  }
+
+
+  function taskcompleted (message){
+    Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: message,
+        showConfirmButton: false,
+        timer: 1500
+      });
 }
 
   const users = new Array(); 
   boton.addEventListener("click", function(event){
     event.preventDefault();
-    let isValid = validarCampos();
     alert.innerHTML = "";
+    let isValid = validarCampos();
 
     if (isValid){
-        let registro = `{"nombre": "${$nombre.value}",
-        "teléfono": "${$telefono.value}","email": "${$email.value}", "contraseña": "${$contraseña.value}"
+        let registro = `{"nombre": "${nombre.value}",
+        "teléfono": "${telefono.value}","email": "${email.value}", "contraseña": "${contraseña.value}"
     }`;
     users.push(JSON.parse(registro)); //Agrega el array al JSON
     localStorage.setItem("users", JSON.stringify(users));
 
     taskcompleted("El registro se ha hecho correctamente");
-    $nombre.value="";
-    $telefono.value="";
-    $email.value="";
-    $contraseña.value="";
-    $contraseñaconf.value="";
-    $nombre.focus();
+    nombre.value="";
+    telefono.value="";
+    email.value="";
+    contraseña.value="";
+    contraseñaconf.value="";
+    nombre.focus();
     }
   });
