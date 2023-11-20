3f1dabffd3fee6b45d0cb2ae413d34101d57c8ef
Karen Valenzuela Landero <11952598+karenvalenzuelalandero@users.noreply.github.com>
2023-11-20 16:16:32 -0600

    localStorage en servicios, usuarios, user, pass, modificación gestión de servicios

--- js/gestion_servicios.js
+++ js/gestion_servicios.js
@@ -1,114 +1,102 @@
-let $alert_container = document.getElementById("alert-container");
-let $service_name = document.getElementById("service_name");
-let $service_description = document.getElementById("service_description");
-let btnImageUpload = document.getElementById("Inputimg");
-let btnsubmit = document.getElementById("submit");
-let btnClear = document.getElementById("btnClear");
+const $alert_container = document.getElementById("alert-container");
+const imgDefault = "./src/upload-image.jpg";
 let services = new Array();
-let infowidget;
+let message ;
 
-//Cloudinary
+function taskcompleted (message){
+    Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: message,
+        showConfirmButton: false,
+        timer: 1500
+      });
+}//taskCompleted
+
+function cleanWarnings(){
+    $alert_container.innerHTML = "";
+    service_name.style.border="";
+    service_description.style.border="";
+    btnUploadWidget.style.border="";
+}//cleanWarnings
+
+function warningAlert(lblStyled, message){
+    lblStyled.style.border="solid thin red";
+    let showalert =
+        `<div class="alert alert-warning alert-dismissible show" role="alert" id="alert">
+            ${message}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" id="close-warning"></button>
+        </div>`;
+    $alert_container.insertAdjacentHTML("beforeend",showalert);
+    //$alert_container.innerHTML = showalert;
+}//warningAlert
+
 var myWidget = cloudinary.createUploadWidget({
     cloudName: 'dz4ctdoqw', 
     uploadPreset: 'my-preset'}, (error, result) => { 
-      if (!error && result && result.event === "success") { 
-        console.log('Done! Here is the image info: ', result.info); 
-        infowidget = result.info; //test
-        console.log(infowidget);  //test
-        return infowidget;  //test
-      }
+        if (!error && result && result.event === "success") { 
+            console.log('Done! Here is the image info: ', result.info); 
+            document.getElementById("uploadedimage").setAttribute("src", result.info.secure_url);
+        }
     }
-  )
-  btnImageUpload.addEventListener("click", function(e){
+);//Cloudinary Widget
+
+btnUploadWidget.addEventListener("click", function(e){
     e.preventDefault();
     myWidget.open();
-}, false);
+}, false);//Cloudinary button
 
+function validateService(service_name, service_description, urlImage){
+    if (service_name.value.length < 10){
+        message = "El nombre debe tener más de 10 caracteres";
+        warningAlert(service_name, message);
+        return false;
+    }   
+    if (service_description.value.length < 20){
+        message = "La descripción debe tener más de 20 caracteres";
+        warningAlert(service_description, message);
+        return false;
+    }   
+    if(urlImage == "" || urlImage == imgDefault)  {   
+        message = "Por favor seleccione una imagen";
+        warningAlert(btnUploadWidget, message);
+        return false;
+    }
+    return true;
+}//validateService
 
-//Submit Data
-btnsubmit.addEventListener("click", function(event){
-  event.preventDefault();
-  let isValid = true;
-  let message = "";
-  cleanWarnings();
-  
-  if ($service_name.value.length < 10){
-      console.log("nomb: "+$service_name.value.length);
-      message = "El nombre debe tener más de 10 caracteres";
-      warningAlert($service_name, message);
-      isValid=false;
-  }   //$service_name < 10
-  if ($service_description.value.length < 20){
-      console.log("desc: "+$service_description.value.length);
-      message = "La descripción debe tener más de 20 caracteres";
-      warningAlert($service_description, message);
-      isValid=false;
-  }   //$service_description < 20
-  if(infowidget == "")  {   
-    console.log("btnimg: "+btnImageUpload.value);//test
-    console.log("cloudinary: "+infowidget);//test
-    message = "Por favor seleccione una imagen";
-    warningAlert(btnImageUpload, message);
-    return false;
-  } 
+btnSubmit.addEventListener("click", function(event){
+    event.preventDefault();
+    cleanWarnings();
+    const $service_name = document.getElementById("service_name");
+    const $service_description = document.getElementById("service_description");
+    const $uploadedimage = document.getElementById("uploadedimage");
+    const urlImage = $uploadedimage.getAttribute("src");
+    const isValid = validateService($service_name, $service_description, urlImage);
+    
+    if (isValid){
+        let service = `{"nombre": "${$service_name.value}",
+            "descripción": "${$service_description.value}",
+            "urlImage": "${urlImage}"
+            }`; //
+        services.push(JSON.parse(service)); 
+        localStorage.setItem("services", JSON.stringify(services)); 
 
-  if (isValid){
-      console.log("nomb: "+$service_name.value.length);
-      console.log("desc: "+$service_description.value.length);
-      console.log("infowidget: "+infowidget);
-
-      let service = `{"nombre": "${$service_name.value}",
-          "descripción": "${$service_description.value}"
-      }`;
-      services.push(JSON.parse(service)); //Agrega al array services el JSON de service
-      localStorage.setItem("services", JSON.stringify(services)); //Agrega al localStorage el array de servicios en String
-
-      taskcompleted("El Servicio ha sido guardado");
-      $service_name.value="";
-      $service_description.value="";
-      $service_name.focus();
-  }
-});
-
-function taskcompleted (message){
-  Swal.fire({
-      position: "top-end",
-      icon: "success",
-      title: message,
-      showConfirmButton: false,
-      timer: 1500
-    });
-}
-
-function cleanWarnings(){
-  $alert_container.innerHTML = "";
-  $service_name.style.border="";
-  $service_description.style.border="";
-}
-
-function warningAlert(lblStyled, message){
-
-  lblStyled.style.border="solid thin red";
-
-  let showalert =
-      `<div class="alert alert-warning alert-dismissible show" role="alert" id="alert">
-          ${message}
-          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" id="close-warning"></button>
-      </div>`;
-  $alert_container.insertAdjacentHTML("beforeend",showalert);
-  //$alert_container.innerHTML = showalert;
-}
+        taskcompleted("Servicio registrado correctamente");
+        $service_name.value="";
+        $service_description.value="";
+        $service_name.focus();
+        $uploadedimage.src = imgDefault;
+    }
+});//btnSubmit
 
 btnClear.addEventListener("click", function(event){
-  event.preventDefault();
-  $alert_container.innerHTML = "";
-  $service_name.value="";
-  $service_description.value="";
-  $service_name.focus();
-  $service_name.style.border="";
-  $service_description.style.border="";
-});
-
-
-
-
+    event.preventDefault();
+    $alert_container.innerHTML = "";
+    service_name.value="";
+    service_description.value="";
+    service_name.focus();
+    service_name.style.border="";
+    service_description.style.border="";
+    uploadedimage.src = imgDefault;
+});//btnClear
