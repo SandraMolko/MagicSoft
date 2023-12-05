509d2f5c9f6d5d28f0136e057251dcf7da4f93e2
Karen Valenzuela Landero <11952598+karenvalenzuelalandero@users.noreply.github.com>
2023-12-05 09:59:22 -0600

    Actualización de la base de datos

--- baseDeDatos/SCRIPT AMR FOR DATABASE.sql
+++ baseDeDatos/SCRIPT AMR FOR DATABASE.sql
@@ -15,39 +15,40 @@ CREATE SCHEMA IF NOT EXISTS `magicsoft` DEFAULT CHARACTER SET utf8 ;
 USE `magicsoft` ;
 
 -- -----------------------------------------------------
--- Table `magicsoft`.`Administradores`
+-- Table `magicsoft`.`administradores`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `magicsoft`.`administradores` (
   `idAdm` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
   `telefono` VARCHAR(10) NOT NULL,
-  `contraseña` VARCHAR(20) NOT NULL,
+  `contrasena` VARCHAR(20) NOT NULL,
   PRIMARY KEY (`idAdm`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `magicsoft`.`Servicios`
+-- Table `magicsoft`.`servicios`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `magicsoft`.`Servicios` (
+CREATE TABLE IF NOT EXISTS `magicsoft`.`servicios` (
   `idServicio` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(100) NOT NULL,
   `descripcion` VARCHAR(300) NOT NULL,
   `imagen` VARCHAR(500) NOT NULL,
-  `Administradores_idAdm` INT NOT NULL,
-  PRIMARY KEY (`idServicio`, `Administradores_idAdm`),
-  INDEX `fk_Servicios_Administradores_idx` (`Administradores_idAdm` ASC) VISIBLE,
-  CONSTRAINT `fk_Servicios_Administradores`
+  `administradores_idAdm` INT NOT NULL,
+  PRIMARY KEY (`idServicio`, `administradores_idAdm`),
+  INDEX `fk_servicios_administradores_idx` (`administradores_idAdm` ASC) ,
     FOREIGN KEY (`administradores_idAdm`)
     REFERENCES `magicsoft`.`administradores` (`idAdm`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `magicsoft`.`Cotizaciones`
+-- Table `magicsoft`.`cotizaciones`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `magicsoft`.`cotizaciones` (
   `idCotizacion` INT NOT NULL AUTO_INCREMENT,
@@ -56,51 +57,33 @@ CREATE TABLE IF NOT EXISTS `magicsoft`.`cotizaciones` (
   `telefono` VARCHAR(10) NOT NULL,
   `empresa` VARCHAR(45) NOT NULL,
   `mensaje` VARCHAR(300) NOT NULL,
-  PRIMARY KEY (`idCotizacion`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `magicsoft`.`Cotizaciones_has_Servicios`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `magicsoft`.`cotizaciones_has_servicios` (
-  `cotizaciones_idCotizacion` INT NOT NULL,
   `servicios_idServicio` INT NOT NULL,
-  `servicios_administradores_idAdm` INT NOT NULL,
-  PRIMARY KEY (`cotizaciones_idCotizacion`, `servicios_idServicio`, `servicios_administradores_idAdm`),
-  INDEX `fk_cotizaciones_has_servicios_servicios_idx` (`servicios_idServicio` ASC, `servicios_administradores_idAdm` ASC) VISIBLE,
-  INDEX `fk_cotizaciones_has_servicios_cotizaciones_idx` (`cotizaciones_idCotizacion` ASC) VISIBLE,
-  CONSTRAINT `fk_cotizaciones_has_servicios_cotizaciones`
-    FOREIGN KEY (`cotizaciones_idCotizacion`)
-    REFERENCES `magicsoft`.`cotizaciones` (`idCotizacion`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_cotizaciones_has_servicios_servicios`
-    FOREIGN KEY (`servicios_idServicio` , `servicios_administradores_idAdm`)
-    REFERENCES `magicsoft`.`servicios` (`idServicio` , `administradores_idAdm`)
+  PRIMARY KEY (`idCotizacion`, `servicios_idServicio`),
+  INDEX `fk_cotizaciones_servicios_idx` (`servicios_idServicio` ASC) ,
+    FOREIGN KEY (`servicios_idServicio`)
+    REFERENCES `magicsoft`.`servicios` (`idServicio`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `magicsoft`.`Ventas`
+-- Table `magicsoft`.`ventas`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `magicsoft`.`ventas` (
   `idVenta` INT NOT NULL AUTO_INCREMENT,
   `precio` FLOAT NOT NULL,
   `fechas` VARCHAR(10) NOT NULL,
-  `cotizaciones_has_servicios_cotizaciones_idCotizacion` INT NOT NULL,
-  `cotizaciones_has_servicios_servicios_idServicio` INT NOT NULL,
-  `cotizaciones_has_servicios_servicios_administradores_idAdm` INT NOT NULL,
-  PRIMARY KEY (`idVenta`, `cotizaciones_has_servicios_cotizaciones_idCotizacion`, `cotizaciones_has_servicios_servicios_idServicio`, `cotizaciones_has_servicios_servicios_administradores_idAdm`),
-  INDEX `fk_ventas_cotizaciones_has_servicios_idx` (`cotizaciones_has_servicios_cotizaciones_idCotizacion` ASC, `cotizaciones_has_servicios_servicios_idServicio` ASC, `cotizaciones_has_servicios_servicios_administradores_idAdm` ASC) VISIBLE,
-  CONSTRAINT `fk_ventas_cotizaciones_has_servicios`
-    FOREIGN KEY (`cotizaciones_has_servicios_cotizaciones_idCotizacion` , `cotizaciones_has_servicios_servicios_idServicio` , `cotizaciones_has_servicios_servicios_administradores_idAdm`)
-    REFERENCES `magicsoft`.`cotizaciones_has_servicios` (`cotizaciones_idCotizacion` , `servicios_idServicio` , `servicios_administradores_idAdm`)
+  `cotizaciones_idCotizacion` INT NOT NULL,
+  PRIMARY KEY (`idVenta`, `cotizaciones_idCotizacion`),
+  INDEX `fk_ventas_cotizaciones_idx` (`cotizaciones_idCotizacion` ASC) ,
+    FOREIGN KEY (`cotizaciones_idCotizacion`)
+    REFERENCES `magicsoft`.`cotizaciones` (`idCotizacion`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
